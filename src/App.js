244184830da49e70347f1b7f54f230b5b7ada19f
ZoeyF75 <zf.zoodle@gmail.com>
2021-05-20 20:58:33 -0700

    First sentence rendered

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const dataFromServer = await fetchData();
+      setData(dataFromServer.data.sentence);
+    }
+    getData();
+  }, []);
+
+  const fetchData= async () => {
+    const res = await fetch('https://api.hatchways.io/assessment/sentences/1');
+    const data = await res.json();
+    return data
+  }
+
   return (
-    <div className="App">
-      Hello
+    <div id="scrambled-word">
+      {data}
     </div>
   );
 }
